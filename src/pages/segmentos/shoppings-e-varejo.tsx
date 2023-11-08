4bac52da8913df7b47636852a12bb07104832312
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-11-08 14:03:08 -0300

    feat: add logistica page with responsiveness

---
+++ src/pages/segmentos/shoppings-e-varejo.tsx
@@ -0,0 +1,55 @@
+import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
+import { BannerSegments } from "@/components/BannerSegments"
+import { Infobox } from "@/components/Infobox"
+
+export default function ShoppingsEVarejo() {
+  const iconSVG = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"></path></svg>`
+  const infoboxes = [
+    {
+      iconSVG: iconSVG,
+      title: "Shoppings",
+    },
+    {
+      iconSVG: iconSVG,
+      title: "Grandes Varejistas",
+    },
+    {
+      iconSVG: iconSVG,
+      title: "Pequenos Negócios",
+    }
+  ]
+
+  return (
+    <>
+      <Header />
+      <div className="relative top-[72px] md:top-[76px] lg:top-[80px]">
+        <BannerSegments banner={{
+          image: { src: "/example.jpg", alt: "" },
+          title: "Shoppings e Varejo",
+          caption: "Soluções eficazes e inovadoras para o segmento varejista",
+          infobox: {
+            title: "Segmento de Shoppings e Varejo",
+            description: "O Grupo Efficiency Service oferece Soluções completas em Segurança e Serviços para Shoppings de todos os portes. Colaboradores treinados, produtos de alta performance e equipamentos com tecnologia de ponta fazem do Grupo Efficiency Service, um parceiro estratégico para o segmento.",
+            image: { src: "/example.jpg", alt: "" },
+          },
+        }} />
+        <main className="max-w-5xl mx-auto py-12 px-4 xl:px-0">
+          <h2 className="text-5xl text-second text-center font-bold pb-6">
+            Soluções para Shopping de todos os portes
+          </h2>
+          <p className="text-lg text-center text-gray-600 pb-6">
+            O Grupo Efficiency Service possui as soluções mais eficientes e inovadoras para shoppings de todos os portes em 13 Estados do país. Limpeza, Segurança, Manobristas, Jardineiros, Fiscais de Piso, Atendentes, uma gama completa de serviços e tecnologia para atender centros comerciais com eficiência.
+          </p>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {infoboxes.map((infobox) => (
+              <Infobox key={infobox.title} infobox={infobox} />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
--- src/pages/segmentos/transporte-e-logistica.tsx
+++ src/pages/segmentos/transporte-e-logistica.tsx
@@ -3,20 +3,20 @@ import { Footer } from "@/components/Footer"
 import { BannerSegments } from "@/components/BannerSegments"
 import { Infobox } from "@/components/Infobox"
 
-export default function Shoppings() {
+export default function TransporteELogistica() {
   const iconSVG = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"></path></svg>`
   const infoboxes = [
     {
       iconSVG: iconSVG,
-      title: "Shoppings",
+      title: "Transportadoras",
     },
     {
       iconSVG: iconSVG,
-      title: "Grandes Varejistas",
+      title: "Armazéns",
     },
     {
       iconSVG: iconSVG,
-      title: "Pequenos Negócios",
+      title: "Centros de Distribuição",
     }
   ]
 
@@ -26,20 +26,20 @@ export default function Shoppings() {
       <div className="relative top-[72px] md:top-[76px] lg:top-[80px]">
         <BannerSegments banner={{
           image: { src: "/example.jpg", alt: "" },
-          title: "Shoppings e Varejo",
-          caption: "Soluções eficazes e inovadoras para o segmento varejista",
+          title: "Transportes e Logística",
+          caption: "Soluções eficazes e Inovadoras para o segmento de Logística",
           infobox: {
-            title: "Segmento de Shoppings e Varejo",
-            description: "O Grupo Efficiency Service oferece Soluções completas em Segurança e Serviços para Shoppings de todos os portes. Colaboradores treinados, produtos de alta performance e equipamentos com tecnologia de ponta fazem do Grupo Efficiency Service, um parceiro estratégico para o segmento.",
+            title: "Segmento de transporte e logística",
+            description: "O setor de logística é um dos que mais crescem no país. Com o desenvolvimento do comércio online, entregar produtos com rapidez e agilidade tornou-se um diferencial importante. O número de transportadoras, galpões e condomínios logísticos também tem aumentado significativamente, assim como a necessidade de proteção desses locais. O Grupo Efficiency Service tem tradição na prestação de serviços de Segurança e Facilities para o setor logístico. Com mais de 30 anos de mercado, atender transportadoras, centros de distribuição, galpões e condomínios logísticos é uma das especialidades da empresa.",
             image: { src: "/example.jpg", alt: "" },
           },
         }} />
         <main className="max-w-5xl mx-auto py-12 px-4 xl:px-0">
           <h2 className="text-5xl text-second text-center font-bold pb-6">
-            Soluções para Shopping de todos os portes
+            Soluções para diversos tipos de Segmentos em Logística
           </h2>
           <p className="text-lg text-center text-gray-600 pb-6">
-            O Grupo Efficiency Service possui as soluções mais eficientes e inovadoras para shoppings de todos os portes em 13 Estados do país. Limpeza, Segurança, Manobristas, Jardineiros, Fiscais de Piso, Atendentes, uma gama completa de serviços e tecnologia para atender centros comerciais com eficiência.
+          O Grupo Efficiency Service possui as soluções mais eficientes e inovadoras para você, grande varejista, que precisa assegurar a segurança das suas mercadorias, colaboradores e clientes. Soluções de monitoramento e controle de acesso inteligente para o seu estoque.
           </p>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
